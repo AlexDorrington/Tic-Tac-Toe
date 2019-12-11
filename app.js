796ec85711ffda64f9dffc6ca101a8fc6904bfce
Helios's Tan <ajdorrington@hotmail.co.uk>
2019-12-11 22:27:57 +0000

    first commit

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const myGameBoard = {
             this.declareWinner(playerSymbol, ({name}) => {
                 winnerText.innerHTML = `Well done ${name}, you won`
             })
-        } else if (this.board[2][1] === playerSymbol && this.board[7][1] === playerSymbol && this.board[6][1] === playerSymbol) {
+        } else if (this.board[2][1] === playerSymbol && this.board[4][1] === playerSymbol && this.board[6][1] === playerSymbol) {
             this.declareWinner(playerSymbol, ({name}) => {
                 winnerText.innerHTML = `Well done ${name}, you won`
             })
